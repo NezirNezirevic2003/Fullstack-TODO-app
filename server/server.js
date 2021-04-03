f82e1a18403799803bc68bae4143b94bf4891f74
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2021-04-03 22:40:25 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -46,6 +46,22 @@ app.get("/todos/:id", async (req, res) => {
   }
 });
 
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("Updated succesfully");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
